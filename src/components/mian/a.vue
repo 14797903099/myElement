08842bb39ea9dddddf20bf78728abd7aa34c4213
zyb <2196159512@qq.com>
2017-07-15 17:52:25 +0800

    试试吗

--- src/components/mian/a.vue
+++ src/components/mian/a.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-     我是啊
+     <!--<div :class="{'class-a':isA, 'class-b':!isA}" @click="handleTab">我是三角阀技术开发及</div>-->
+     <ul>
+       <li v-for='item in data' v-text="item.name" :class="{cur:item.iscur}" @click="setCur($index)"></li>
+     </ul> 
   </div>
 </template>
 
@@ -10,14 +13,31 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      isA:false
     }
+  },
+  methods: {
+    handleTab () {
+      this.isA = !this.isA
+    },
+    setCur (index) {
+      this.data.map(function (v,i) {
+          i==index? v.iscur=true: v.iscur=false;
+      });
+    } 
   }
 }
 </script>
 	
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less">
+.class-a{
+  color: red
+}
+.class-b{
+  color: green
+}
 .hello{
 	width: 100%;
 	height: 100%;
